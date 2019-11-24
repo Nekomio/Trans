feb31035c1415d01a852ac066013780c3bc0f957
Sadam <1903249375@qq.com>
2019-11-24 10:25:32 +0800

    Initial commit

---
+++ AIR_System/view.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def log():
+    return HttpResponse("this is")
---
+++ back_stage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BackStageConfig(AppConfig):
+    name = 'back_stage'
