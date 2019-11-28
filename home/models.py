b7eafbc8bc12c1458fe6ef83aca293f1f5bbea56
Sadam <1903249375@qq.com>
2019-11-28 14:06:53 +0800

    1.has created the Model:Schoolfellow
    Signed-off-by: Sadam <1903249375@qq.com>

--- home/models.py
+++ home/models.py
@@ -1,1 +1,24 @@
 # Create your models here.
+from django.db import models
+
+
+class SchoolFellow(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(name="姓名", max_length=30, null=False)
+    sex = models.BooleanField(name="性别", null=False)
+    tell = models.CharField(name="联系方式", max_length=11, null=False)
+    email = models.EmailField(name="电子邮箱", null=False)
+    department = models.CharField(name="院系", max_length=100, null=False)
+    school_class = models.CharField(name="班级", max_length=30, null=True)
+    education = models.CharField(name="学历", max_length=50, null=False, default="本科学生")
+    year = models.IntegerField(name="学制", null=True)
+    start = models.DateField(name="入学年份", null=False)
+    graduate = models.DateField(name="毕业年份", null=False)
+    teacher = models.CharField(name="辅导员老师或印象最深刻的任课老师", max_length=40, null=True)
+    work_place = models.CharField(name="现工作单位", max_length=100, null=False)
+    address = models.CharField(name="工作单位地址", max_length=200, null=True)
+    category = models.CharField(name="行业类别", max_length=20, null=True)
+    work_property = models.CharField(name="单位性质", max_length=30, null=True)
+    title = models.CharField(name="现职务职称", max_length=100, null=True)
+    honour = models.TextField(name="所获荣誉", null=True)
+    comments = models.TextField(name="备注", null=True)
