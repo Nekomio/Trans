568866e7f3928fc1aed589d1806682e70d869345
Sadam <1903249375@qq.com>
2019-12-05 00:36:59 +0800

    do some changes for online deployment: 1.turn off the debug mod, which lead to the default static file service has been shut down. 2.set up the url and the static file services. 3.the img of the check code save into the dir of STATIC_ROOT each times. 4.fixed the bug of view func : get_excel, when a ordinary user but not a super user also can get the excel. By login_required and judging whether the user of request is super user or not, it is easily fixed.
    Signed-off-by: Sadam <1903249375@qq.com>

--- AIR_System/urls.py
+++ AIR_System/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views import static
+
+from AIR_System.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    url('^static/(?P<path>.*)$', static.serve, {'document_root': STATIC_ROOT}, name='static')
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,15 @@
+import os
 from datetime import datetime
 from io import BytesIO
 
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from xlwt import Workbook, XFStyle
 
+from AIR_System.settings import STATIC_ROOT
 from config import fields
 from home.models import Account, SchoolFellow
 from utils.ChekCode import CheckCode
@@ -20,6 +22,7 @@ def register(request):
     session_key = str(hash(request.META['REMOTE_ADDR']))
     src = "%s.png" % session_key
     resp['passcode_src'] = src
+    path = os.path.join(STATIC_ROOT, src)
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -45,13 +48,13 @@ def register(request):
                 resp['error_msg'] = '用户名或密码不能为空，请正确输入'
         resp['name'] = username
         resp['password'] = password
-        passcode = Obj_code.gene_code("./home/static/%s" % src)
+        passcode = Obj_code.gene_code(path)
         print(session_key, passcode)
         request.session[session_key] = passcode
         return render(request, 'register.html', resp)
 
     if request.method == "GET":
-        passcode = Obj_code.gene_code("./home/static/%s" % src)
+        passcode = Obj_code.gene_code(path)
         print(session_key, passcode)
         request.session[session_key] = passcode
         return render(request, 'register.html', resp)
@@ -62,6 +65,7 @@ def login(request):
     session_key = str(hash(request.META['REMOTE_ADDR']))
     src = "%s.png" % session_key
     resp['passcode_src'] = src
+    path = os.path.join(STATIC_ROOT, src)
     if request.method == "POST":
         print("has printed：post:", request.POST)
         username = request.POST['username']
@@ -84,7 +88,7 @@ def login(request):
                     resp['error_msg'] = "密码错误"
             else:
                 resp['error_msg'] = "用户不存在"
-        passcode = Obj_code.gene_code("./home/static/%s" % src)
+        passcode = Obj_code.gene_code(path)
         print(session_key, passcode)
         request.session[session_key] = passcode
         resp['name'] = username
@@ -96,7 +100,7 @@ def login(request):
         # print(users)
     if request.method == "GET":
         # print("has printed GET:", request.GET)
-        passcode = Obj_code.gene_code("./home/static/%s" % src)
+        passcode = Obj_code.gene_code(path)
         print(session_key, passcode)
         request.session[session_key] = passcode
         return render(request, "login.html", resp)
@@ -165,40 +169,45 @@ def information_filling(request):
         return render(request, 'form.html', resp)
 
 
+# @permission_required(perm="")
+@login_required(login_url="user.login")
 def get_excel(request):
-    fellows = SchoolFellow.objects.order_by('name')
-    ws = Workbook(encoding="utf-8")
-    w = ws.add_sheet(u"校友信息导出表")
-    for i in fields:
-        w.write(0, fields.index(i), i)
-    date_format = XFStyle()
-    date_format.num_format_str = 'yyyy-mm-dd'
-    for i in range(len(fellows)):
-        w.write(i + 1, 0, fellows[i].name)
-        w.write(i + 1, 1, fellows[i].sex)
-        w.write(i + 1, 2, fellows[i].tell)
-        w.write(i + 1, 3, fellows[i].fixed_tell)
-        w.write(i + 1, 4, fellows[i].email)
-        w.write(i + 1, 5, fellows[i].department)
-        w.write(i + 1, 6, fellows[i].school_class)
-        w.write(i + 1, 7, fellows[i].education)
-        w.write(i + 1, 8, fellows[i].year_system)
-        # x = datetime.strftime(fellows[i].入学年份, '%Y-%m-%d')
-        w.write(i + 1, 9, fellows[i].year_enroll, date_format)
-        w.write(i + 1, 10, fellows[i].year_graduate, date_format)
-        w.write(i + 1, 11, fellows[i].teacher)
-        w.write(i + 1, 12, fellows[i].mentor)
-        w.write(i + 1, 13, fellows[i].current_work_unit)
-        w.write(i + 1, 14, fellows[i].address_work_unit)
-        w.write(i + 1, 15, fellows[i].industry_category)
-        w.write(i + 1, 16, fellows[i].unit_property)
-        w.write(i + 1, 17, fellows[i].current_job_title)
-        w.write(i + 1, 18, fellows[i].honour)
-        w.write(i + 1, 19, fellows[i].remark)
-    sio = BytesIO()
-    ws.save(sio)
-    sio.seek(0)
-    res = HttpResponse(sio.getvalue(), content_type="application/vnd.ms-excel")
-    sio.close()
-    res['Content-Disposition'] = 'attachment;filename = school_fellow_information_export_table.xls'
-    return res
+    if request.user.is_superuser:
+        fellows = SchoolFellow.objects.order_by('name')
+        ws = Workbook(encoding="utf-8")
+        w = ws.add_sheet(u"校友信息导出表")
+        for i in fields:
+            w.write(0, fields.index(i), i)
+        date_format = XFStyle()
+        date_format.num_format_str = 'yyyy-mm-dd'
+        for i in range(len(fellows)):
+            w.write(i + 1, 0, fellows[i].name)
+            w.write(i + 1, 1, fellows[i].sex)
+            w.write(i + 1, 2, fellows[i].tell)
+            w.write(i + 1, 3, fellows[i].fixed_tell)
+            w.write(i + 1, 4, fellows[i].email)
+            w.write(i + 1, 5, fellows[i].department)
+            w.write(i + 1, 6, fellows[i].school_class)
+            w.write(i + 1, 7, fellows[i].education)
+            w.write(i + 1, 8, fellows[i].year_system)
+            # x = datetime.strftime(fellows[i].入学年份, '%Y-%m-%d')
+            w.write(i + 1, 9, fellows[i].year_enroll, date_format)
+            w.write(i + 1, 10, fellows[i].year_graduate, date_format)
+            w.write(i + 1, 11, fellows[i].teacher)
+            w.write(i + 1, 12, fellows[i].mentor)
+            w.write(i + 1, 13, fellows[i].current_work_unit)
+            w.write(i + 1, 14, fellows[i].address_work_unit)
+            w.write(i + 1, 15, fellows[i].industry_category)
+            w.write(i + 1, 16, fellows[i].unit_property)
+            w.write(i + 1, 17, fellows[i].current_job_title)
+            w.write(i + 1, 18, fellows[i].honour)
+            w.write(i + 1, 19, fellows[i].remark)
+        sio = BytesIO()
+        ws.save(sio)
+        sio.seek(0)
+        res = HttpResponse(sio.getvalue(), content_type="application/vnd.ms-excel")
+        sio.close()
+        res['Content-Disposition'] = 'attachment;filename = school_fellow_information_export_table.xls'
+        return res
+    else:
+        return HttpResponse("Permission denied.")
